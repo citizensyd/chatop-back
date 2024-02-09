580169e7059af7c97d36149f62b7dfad80fc2d5c
citizensyd <59608029+citizensyd@users.noreply.github.com>
2024-02-09 15:04:32 +0100

    Fix - route message

--- src/main/java/com/chatapp/backend/controller/MessageController.java
+++ src/main/java/com/chatapp/backend/controller/MessageController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/messages")
+@RequestMapping("/api/messages/")
 @RequiredArgsConstructor
 public class MessageController {
 
